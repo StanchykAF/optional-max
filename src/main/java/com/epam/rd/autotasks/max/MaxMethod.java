791b8991692eeb8bed123d940ebc74c77556553d
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2021-10-21 13:40:29 +0300

    Optional introduction

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,7 +1,9 @@
 package com.epam.rd.autotasks.max;
 
+import java.util.OptionalInt;
+
 public class MaxMethod {
-    public static int max(int[] values) {
+    public static OptionalInt max(int[] values) {
         throw new UnsupportedOperationException();
     }
 }
--- src/test/java/com/epam/rd/autotasks/max/MaxMethodTests.java
+++ src/test/java/com/epam/rd/autotasks/max/MaxMethodTests.java
@@ -10,27 +10,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class MaxMethodTests {
 
     @Test
+    @DisplayName("For an empty array returns empty result")
+    void testEmptyArray() {
+        OptionalInt result = MaxMethod.max(new int[]{});
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    @DisplayName("For null returns empty result")
+    void testNullArray() {
+        OptionalInt result = MaxMethod.max(null);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
     @DisplayName("For an array of length 1 returns its only value")
     void testReturnArraysSingleValue() {
         int[] values = new int[]{10};
-        int result = MaxMethod.max(values);
-        assertEquals(values[0], result);
+        OptionalInt result = MaxMethod.max(values);
+        assertEquals(values[0], result.getAsInt());
     }
 
     @Test
     @DisplayName("For an array of negative ints returns correct max")
     void testReturnMaxForNegativeValues() {
         int[] values = new int[]{-4, -3, -3, -10, -2, -145};
-        int result = MaxMethod.max(values);
-        assertEquals(-2, result);
+        OptionalInt result = MaxMethod.max(values);
+        assertEquals(-2, result.getAsInt());
     }
 
     @Test
     @DisplayName("For an array containing MAX_VALUE return the value")
     void testReturnIntegerMaxIfArrayContainsIt() {
         int[] values = new int[]{-10, 3, 255528, 0, Integer.MAX_VALUE, 100, 0};
-        int result = MaxMethod.max(values);
-        assertEquals(Integer.MAX_VALUE, result);
+        OptionalInt result = MaxMethod.max(values);
+        assertEquals(Integer.MAX_VALUE, result.getAsInt());
     }
 
     @Test
@@ -39,28 +53,28 @@ class MaxMethodTests {
         int diff = 27;
         int toFind = Integer.MAX_VALUE - diff;
         int[] values = new int[]{8332, 0, -123, 44, 23, 924823, toFind};
-        int result = MaxMethod.max(values);
-        assertEquals(toFind, result);
+        OptionalInt result = MaxMethod.max(values);
+        assertEquals(toFind, result.getAsInt());
     }
 
     @Test
     void testReturnMaxValueForSampleArray() {
         int[] values = new int[]{-574, -484, 444, -217, 978, -370, 269, -9, 189};
-        int result = MaxMethod.max(values);
-        assertEquals(978, result);
+        OptionalInt result = MaxMethod.max(values);
+        assertEquals(978, result.getAsInt());
     }
 
     @Test
     void testReturnMaxValueForSampleArray2() {
         int[] values = new int[]{-794, -206, -500, -410, -850, -249, -968, -536, -61, -65, -866};
-        int result = MaxMethod.max(values);
-        assertEquals(-61, result);
+        OptionalInt result = MaxMethod.max(values);
+        assertEquals(-61, result.getAsInt());
     }
 
     @Test
     void testReturnMaxValueForSampleArray3() {
         int[] values = new int[]{806, 19, 140, 392, 36, 528, 351, 29, 786, 646, 656, 542, 890, 424};
-        int result = MaxMethod.max(values);
-        assertEquals(890, result);
+        OptionalInt result = MaxMethod.max(values);
+        assertEquals(890, result.getAsInt());
     }
 }
