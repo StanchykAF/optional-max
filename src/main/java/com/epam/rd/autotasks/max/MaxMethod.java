fd64481b347eb0f5a381edcd574f065a07ad6939
Nastassia Stanchyk <stanchikaf@gmail.com>
2022-05-11 20:50:50 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -4,6 +4,19 @@ import java.util.OptionalInt;
 
 public class MaxMethod {
     public static OptionalInt max(int[] values) {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        OptionalInt optionalInt;
+        if (values != null && values.length > 0) {
+            int maxValue = values[0];
+            for (int elem : values) {
+                if (maxValue < elem) {
+                    maxValue = elem;
+                }
+            }
+            optionalInt = OptionalInt.of(maxValue);
+        } else {
+            optionalInt = OptionalInt.empty();
+        }
+        return optionalInt;
     }
 }
